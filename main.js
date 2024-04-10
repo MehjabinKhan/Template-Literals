e9d29e2dffb6cc0bcf0faf77c0fc39c24e128403
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-04-10 13:30:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+// Template Literals
+let firstName = "Mehjabin";
+let lastName = "Khan";
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+let yourAge = 30;
+let yourHeight = 5.5;
+let weightInKg = 50;
+let heightInMeters = 5.5;
+let yourBmi = weightInKg / (heightInMeters * heightInMeters);
+//console.log(yourBmi);
+console.log('My age is' + ' ' + yourAge + ' ' + 'Height is approximately' + ' ' + yourHeight + ' ' + 'Now my BMI is' + ' ' + yourBmi);
+let workingOn = "New projects";
+let timeDuration = "2.5 hours";
+console.log('Today woking on' + ' ' + workingOn + ' ' + 'have to complete in' + ' ' + timeDuration);
+let learning = "Typescript";
+let reason = "likes to do coding";
+console.log(learning + ' ' + reason);
---
+++ main.ts
@@ -0,0 +1,21 @@
+// Template Literals
+let firstName: string = "Mehjabin";
+let lastName: string = "Khan";
+let fullName: string = firstName + ' ' + lastName;
+console.log(fullName);
+
+let yourAge = 30;
+let yourHeight = 5.5;
+let weightInKg = 50;
+let heightInMeters = 5.5;
+let yourBmi = weightInKg / (heightInMeters * heightInMeters);
+//console.log(yourBmi);
+console.log('My age is' + ' ' + yourAge + ' ' + 'Height is approximately' + ' ' + yourHeight + ' ' + 'Now my BMI is' + ' ' + yourBmi);
+
+let workingOn = "New projects";
+let timeDuration = "2.5 hours";
+console.log('Today woking on' + ' ' + workingOn + ' ' + 'have to complete in' + ' ' + timeDuration);
+
+let learning = "Typescript";
+let reason = "likes to do coding";
+console.log(learning + ' ' + reason);
